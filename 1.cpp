ee6212b15dd98ad1351b50de717b870fa08fcdac
Shashank <shashank.d0205@gmail.com>
2023-05-07 20:57:02 +0530

    done

---
+++ 1.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int t,x,y;
+    cin>>t;
+    while(t--){
+        cin>>x>>y;
+        if (x*x*x*x + 4*y*y = 4*x*x*y){
+            cout<<"YES\n";
+        }
+        else{
+            cout<<"NO\n";
+        }
+    }
+	return 0;
+}
+
+
