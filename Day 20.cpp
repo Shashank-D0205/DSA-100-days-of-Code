d4fa1bdbde5f4dec57f144e941d7b584112f7a63
Shashank Dixit <106923847+Shashank-D0205@users.noreply.github.com>
2023-10-13 22:18:41 +0530

    Add files via upload

---
+++ Day 20.cpp
@@ -0,0 +1,10 @@
+//Divide two integers
+class Solution {
+public:
+    int divide(int dividend, int divisor) {
+        if(dividend == INT_MIN && divisor == -1){
+            return INT_MAX;
+        }
+        return dividend/divisor;
+    }
+};
