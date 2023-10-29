0f0a4a9fe774de81290a7f060d37bd610ce952b2
Shashank Dixit <106923847+Shashank-D0205@users.noreply.github.com>
2023-10-29 22:11:13 +0530

    Add files via upload

---
+++ Day 35.cpp
@@ -0,0 +1,16 @@
+//Climbing stairs
+class Solution {
+public:
+    int climbStairs(int n) {
+        if(n == 0 || n==1){
+            return 1;
+        }
+        int prev =1 , current =1;
+        for(int i =2;i<=n;i++){
+            int temp =current;
+            current = prev + current;
+            prev = temp;
+        }
+        return current;
+    }
+};
