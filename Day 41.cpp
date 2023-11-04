45e0aed644690ecd84032e14be7ef3568fc5790a
Shashank Dixit <106923847+Shashank-D0205@users.noreply.github.com>
2023-11-04 21:55:07 +0530

    Add files via upload

---
+++ Day 41.cpp
@@ -0,0 +1,14 @@
+//Last moments before all ants fall out of plank
+class Solution {
+public:
+    int getLastMoment(int n, vector<int>& left, vector<int>& right) {
+        int t=0;
+        for(int pos : left){
+            t = max(t,pos);
+        }
+        for(int pos : right){
+            t=max(t,n-pos);
+        }
+        return t;
+    }
+};
