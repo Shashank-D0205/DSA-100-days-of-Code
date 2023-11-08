355cb8500e314cf52e77bbeb2214003746209279
Shashank Dixit <106923847+Shashank-D0205@users.noreply.github.com>
2023-11-08 22:49:02 +0530

    Add files via upload

---
+++ Day 45.cpp
@@ -0,0 +1,12 @@
+//IF a cell is reachable at a given time t
+class Solution {
+public:
+    bool isReachableAtTime(int sx, int sy, int fx, int fy, int t) {
+        int x = abs(sx-fx);
+        int y = abs(sy-fy);
+        if(x == 0 && y == 0){
+            return t != 1;
+        }
+        return x <= t && y<=t;
+    }
+};
