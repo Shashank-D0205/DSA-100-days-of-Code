0a325041fcc03486a28a6f800fe2319f444f6fbf
Shashank Dixit <106923847+Shashank-D0205@users.noreply.github.com>
2023-11-15 22:31:40 +0530

    Add files via upload

---
+++ Day 51.cpp
@@ -0,0 +1,16 @@
+//SINGLE NUMBER II 
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        unordered_map<int ,int> map;
+        for(auto x: nums){
+            map[x]++;
+        }
+        for(auto x: map){
+            if(x.second == 1){
+                return x.first;
+            }
+        }
+        return -1;
+    }
+};
