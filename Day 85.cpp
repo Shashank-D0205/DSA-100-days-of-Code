90424e7adbcbbe6202e6159edc60bb516acff3d8
Shashank Dixit <106923847+Shashank-D0205@users.noreply.github.com>
2024-02-17 10:33:08 +0530

    Add files via upload

---
+++ Day 85.cpp
@@ -0,0 +1,33 @@
+//Palindrome Partitioning
+class Solution {
+public:
+    bool valid(string s){
+        int n=s.size();
+        for(int i=0;i<n/2;i++){
+            if(s[i] != s[n-i-1]){
+                return false;
+            }
+        }
+        return true;
+    }
+    void helper(int start,string &s,vector<string> &temp,vector<vector<string>> &ans){
+        if(start>=s.length()){
+            ans.push_back(temp);
+            return;
+        }
+        for(int end=start;end<s.length();end++){
+            string current = s.substr(start,end-start+1);
+            if(valid(current)){
+                temp.push_back(current);
+                helper(end+1,s,temp,ans);
+                temp.pop_back();
+            }
+        }
+    }
+    vector<vector<string>> partition(string s) {
+        vector<vector<string>> ans;
+        vector<string> temp;
+        helper(0,s,temp,ans);
+        return ans;
+    }
+};
